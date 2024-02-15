6fe925c8e34259be22f4c0b77d7ef5c3c5bac1c5
terukouki07 <azumacha08@gmail.com>
2024-02-15 08:41:51 +0000

    [start]active_storage

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -71,6 +71,9 @@ Rails.application.configure do
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
+  #active_storageの画像変更
+  config.active_job.queue_adapter = :inline
+
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
 
