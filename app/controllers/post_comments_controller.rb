1f7c0bc8b8b6734bdfcc095c7bde3694dd1c05e8
terukouki07 <azumacha08@gmail.com>
2024-02-18 14:09:34 +0000

    [finish]comment_destroy

--- app/controllers/post_comments_controller.rb
+++ app/controllers/post_comments_controller.rb
@@ -9,6 +9,8 @@ class PostCommentsController < ApplicationController
   end
 
   def destroy
+    PostComment.find(params[:id]).destroy
+    redirect_to post_image_path(params[:post_image_id])
   end
 
   private
