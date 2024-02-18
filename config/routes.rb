650b03ec7038868050bf3c39874c7e62a09be98f
terukouki07 <azumacha08@gmail.com>
2024-02-18 14:03:02 +0000

    [finish]comment_create

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :post_images, only: [:new, :create, :index, :show, :destroy]
+  resources :post_images, only: [:new, :create, :index, :show, :destroy] do
+    resources :post_comments, only: [:create, :destroy]
+  end
   resources :users, only: [:show, :edit, :update]
   root to: 'homes#top'
   get "homes/about" => "homes#about", as: "about"
