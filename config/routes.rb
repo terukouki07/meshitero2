58c6256f0caff865585cd8f4af5f21eb2296506f
terukouki07 <azumacha08@gmail.com>
2024-02-19 12:20:11 +0000

    [add]routesの設定

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   resources :post_images, only: [:new, :create, :index, :show, :destroy] do
     resources :post_comments, only: [:create, :destroy]
+    resource :favorite, only: [:create, :destroy]
   end
   resources :users, only: [:show, :edit, :update]
   root to: 'homes#top'
