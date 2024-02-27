00129b9237ee53788f15cf513261521909f8e5b9
terukouki07 <azumacha08@gmail.com>
2024-02-27 12:48:33 +0000

    [add]follow

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,12 +3,12 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-         
+
   has_many :post_images, dependent: :destroy
   has_many :post_comments, dependent: :destroy
   has_many :favorites, dependent: :destroy
   has_one_attached :profile_image
-  
+
   #フォローのアソシエーション
   has_many :active_relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
   has_many :passive_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
@@ -16,19 +16,19 @@ class User < ApplicationRecord
   has_many :followings, through: :active_relationships, source: :followed
   #フォロワーを取得
   has_many :followers, through: :passive_relationships, source: :follower
-  
+
   def follow(user)
-    active_relationhsips.create(followed_id: user.id)
+    active_relationships.create(followed_id: user.id)
   end
-  
+
   def unfollow(user)
     active_relationships.find_by(followed_id: user.id).destroy
   end
-  
+
   def following?(user)
     followings.include?(user)
   end
-  
+
   def get_profile_image(width, height)
     unless profile_image.attached?
       file_path = Rails.root.join('app/assets/images/sample-author1.jpg')
