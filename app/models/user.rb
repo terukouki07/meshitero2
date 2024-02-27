1fe38c1b11c7fcd5f5d7765107c7dcffd47e5027
terukouki07 <azumacha08@gmail.com>
2024-02-27 11:45:43 +0000

    [finish]フォローのアソシエーション

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,6 +9,14 @@ class User < ApplicationRecord
   has_many :favorites, dependent: :destroy
   has_one_attached :profile_image
   
+  #フォローのアソシエーション
+  has_many :active_relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
+  has_many :passive_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
+  #フォロー中のuserを取得
+  has_many :followings, through: :active_relationships, source: :followed
+  #フォロワーを取得
+  has_many :followers, through: :passive_relationships, source: :follower
+  
   def get_profile_image(width, height)
     unless profile_image.attached?
       file_path = Rails.root.join('app/assets/images/sample-author1.jpg')
