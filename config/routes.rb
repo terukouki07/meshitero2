ba9fb597bf3f6963817e9f271b6343190ac4e7b7
terukouki07 <azumacha08@gmail.com>
2024-02-27 12:41:31 +0000

    [remove]routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -5,10 +5,10 @@ Rails.application.routes.draw do
     resource :favorite, only: [:create, :destroy]
   end
   resources :users, only: [:show, :edit, :update] do
-    resources :relationships, only: [:create, :destroy]
-    get 'followings' => 'relationships#followings', as: 'followings'
-    get 'followers' => 'relationships#followers', as: 'unfollows'
-  end
+		resource :relationships, only: [:create, :destroy]
+		get 'followings' => 'relationships#followings', as: 'followings'
+		get 'followers' => 'relationships#followers', as: 'followers'
+	end
   root to: 'homes#top'
   get "homes/about" => "homes#about", as: "about"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
